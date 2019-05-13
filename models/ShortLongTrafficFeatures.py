9aff843589bcf022c1b8124653f54c0f8ac875a3
marina <kornij4uk@gmail.com>
2019-05-13 08:59:52 +0300

    Add ShortTermLSTM layer stracture and forward fuction logic

--- models/ShortLongTrafficFeatures.py
+++ models/ShortLongTrafficFeatures.py
@@ -1,28 +1,53 @@
+import torch
 import torch.nn as nn
 
 
-class TrafficFeatureLSTM (nn.Module):
+class ShortTermLSTM(nn.Module):
 
-    def __init__(self):
-        super(TrafficFeatureLSTM, self).__init__()
-        self.build()
+    def __init__(self, ):
+        super(ShortTermLSTM, self).__init__()
 
-    def build(self):
-        pass
+        self.rnn_0 = nn.LSTM(
+            input_size=4,
+            hidden_size=100,
+            num_layers=1,
+            batch_first=True
+        )
 
-    def forward(self, *input):
-        pass
+        self.rnn_1 = nn.LSTM(
+            input_size=4,
+            hidden_size=100,
+            num_layers=1,
+            batch_first=True
+        )
 
+        self.rnn_2 = nn.LSTM(
+            input_size=4,
+            hidden_size=100,
+            num_layers=1,
+            batch_first=True
+        )
 
-class ShortTermLSTM(TrafficFeatureLSTM):
+    def forward(self, short_ttf):
 
-    def __init__(self, d_n):
-        super(TrafficFeatureLSTM, self).__init__()
-        self.d_n = d_n
+        V_short = []
 
-    def forward(self, temporal, spatial, short_ttf):
-        pass
+        for cell in short_ttf:
 
+            input_0, input_1, input_2 = cell[0], cell[1], cell[2]
+            input_0 = torch.unsqueeze(input_0, dim=0)
+            input_1 = torch.unsqueeze(input_1, dim=0)
+            input_2 = torch.unsqueeze(input_2, dim=0)
+
+            outputs_0, (h_n, c_n) = self.rnn_0(input_0)
+            outputs_1, (h_n, c_n) = self.rnn_1(input_1)
+            outputs_2, (h_n, c_n) = self.rnn_2(input_2)
+
+            hiddens_v = torch.squeeze(torch.cat([outputs_0[:, -1], outputs_1[:, -1], outputs_2[:, -1]], dim=1), dim=0)
+
+            V_short.append(hiddens_v)
+
+        return V_short
 
 class LongTermLSTM(TrafficFeatureLSTM):
     pass
