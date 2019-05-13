c876f2f324b12ee07fac43fdd02a1775476433e0
marina <kornij4uk@gmail.com>
2019-05-13 09:00:44 +0300

    Build final V vector for each cell in the path

--- models/DeepTravel.py
+++ models/DeepTravel.py
@@ -22,12 +22,17 @@ class DeepTravel(nn.Module):
         self(stats, temporal, spatial, dr_state, short_ttf, long_ttf)
 
     def forward(self, stats, temporal, spatial, dr_state, short_ttf, long_ttf):
-        V_sp, V_tp = self.spatial_temporal(stats, temporal, spatial)
+        V_sp, V_tp = self.spatial_temporal(stats, temporal, spatial)    # [path_len, 10]; [path_len, 12]
 
-        V_dri = dr_state
+        V_dri = dr_state.view(-1, 4)                                    # [path_len, 4]
+        V_short = self.short_term_lstm(short_ttf)                       # [300] for each cell
+        V_long = self.long_term_lstm(long_ttf)                          # [100] for each cell
 
-        V_short = self.short_term_lstm(short_ttf)
-        V_long = self.long_term_lstm(long_ttf)
+        V_cells = []
+        for i in range(len(short_ttf)):
+            V_cells.append(torch.cat([V_sp[0], V_tp[0], V_dri[0], V_short[0], V_long[0]]))      # path_len x [426]
+
+        return V_cells
 
     def init_weight(self):
         for name, param in self.named_parameters():
